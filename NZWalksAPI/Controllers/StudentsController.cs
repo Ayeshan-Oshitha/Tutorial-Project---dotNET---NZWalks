f9462b5549740033557741b950f9f1347d5e385b
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2024-02-22 14:46:09 +0530

    2.6 - Running and Calling ASP.NET Core WebAPI

---
+++ NZWalksAPI/Controllers/StudentsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalksAPI.Controllers
+{
+
+    //https://localhost:portnumber/api/Students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+
+        //GET: https://localhost:portnumber/api/Students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+
+            string[] StudentNames = new string[] { "John", "Jane", "Mark", "Emily", "David" };
+
+            //OK means return successful response in ASP.NET Core
+            return Ok(StudentNames);
+
+        }
+    }
+}

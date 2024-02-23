c4e6882bd176f3fc2822c8e88be69f8407ecf887
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2024-02-23 09:28:09 +0530

    2.10 - creating the DBcontext Class

---
+++ NZWalksAPI/Data/NZWalksDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalksAPI.Models.Domain;
+
+namespace NZWalksAPI.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+
+
+        //Here we should pass Db options, because we later need to send our own connection through the program.cs
+        public NZWalksDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) 
+        {
+            
+        }
+
+        //Dbset is a property of Dbcontext class that represents the collection of entities in the database
+        public DbSet<Difficulty> Difficulties  { get; set; }
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Walk> Walks { get; set; }
+    }
+}

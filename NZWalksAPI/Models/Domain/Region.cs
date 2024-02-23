5d9e65044fa6a64cf5a8334f0be1ee6b9babd6c5
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2024-02-23 09:10:43 +0530

    2.8 - Creating Domain Models

---
+++ NZWalksAPI/Models/Domain/Region.cs
@@ -0,0 +1,16 @@
+﻿namespace NZWalksAPI.Models.Domain
+{
+    public class Region
+    {
+
+        public Guid Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+
+        // '?' means , that this property can be a nullable one 
+        public string? RegionImageUrl { get; set; }
+    }
+}

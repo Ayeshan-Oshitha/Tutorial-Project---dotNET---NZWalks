2a17d8cb19ba09dd9f389eebf75637b7aba4517d
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2024-02-23 18:55:52 +0530

    3.2 - Create New Controller (Regions Controller ) - HardCoded Way

---
+++ NZWalksAPI/Controllers/RegionsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalksAPI.Models.Domain;
+
+namespace NZWalksAPI.Controllers
+{
+    // https:// localhost:1234 /api/regions
+    [Route("api/[controller]")] //api/[controller] can be replaced by api/regions
+
+    [ApiController]  // Tells taht, This Controller is for API use
+
+
+
+    //action methods - to create , read, update, delete
+    public class RegionsController : ControllerBase
+        
+    {
+
+        //GET ALL REGIONS
+        // GET: https//localhost:portnumber/api/regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+
+            var regions = new List<Region>
+            {
+                new Region
+                {
+                    Id=Guid.NewGuid(),
+                    Name="Auckland Region",
+                    Code="AKL",
+                    RegionImageUrl="https://images.pexels.com/photos/5342978/pexels-photo-5342978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                },
+
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name="Wellington Region",
+                    Code="WLG",
+                    RegionImageUrl="https://images.pexels.com/photos/710263/pexels-photo-710263.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                }
+            };
+
+            return Ok(regions);
+
+        }
+    }
+}

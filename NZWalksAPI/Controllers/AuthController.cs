6480f7d804f6a72945e73c735603372a6daadd3c
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2024-02-29 13:10:32 +0530

    8.11 - Create JWT Token in ASP.NET Core Web API

--- NZWalksAPI/Controllers/AuthController.cs
+++ NZWalksAPI/Controllers/AuthController.cs
@@ -66,7 +66,7 @@ namespace NZWalksAPI.Controllers
                 {
 
                     //Create Token
-                    return Ok();
+                    
                 }
             }
 

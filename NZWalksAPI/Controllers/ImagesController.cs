64d309a92599b4a3bf8050f631b24f9e54e25a87
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2024-02-29 16:39:17 +0530

    9.4 - New Controller and Action Method for  Image Upload

---
+++ NZWalksAPI/Controllers/ImagesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalksAPI.Models.DTO;
+
+namespace NZWalksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+
+
+        // POST: /api/Images/Upload
+        [HttpPost]
+        [Route("Upload")]
+        
+        public async Task<IActionResult> Upload([FromForm] ImageUploadRequestDto imageUploadRequestDto)
+        {
+            ValidateFileUpload(imageUploadRequestDto);
+
+            if(ModelState.IsValid)
+            {
+                // User repository to upload image
+            }
+
+            return BadRequest(ModelState);
+        }
+
+
+        private void ValidateFileUpload(ImageUploadRequestDto imageUploadRequestDto)
+        {
+            var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png" };
+
+            if(!allowedExtensions.Contains(Path.GetExtension(imageUploadRequestDto.File.FileName)))   //Here File measn IFORMFile in the imageUpdateRequestDto , Filename is the checking property, not a property of a imageUpdateRequestDto class
+            {
+                ModelState.AddModelError("file", "Unsupported file extension");
+            }
+
+
+            if(imageUploadRequestDto.File.Length > 10485760)    //Here File measn IFORMFile in the imageUpdateRequestDto , Length is the checking property, not a property of a imageUpdateRequestDto class
+            {
+                ModelState.AddModelError("file", "File size is more than 10MB, Please upload a smaller file");
+            }
+        }
+    }
+}
